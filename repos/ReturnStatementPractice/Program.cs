7d6fb21bfae210c5c7d1286768a2556b0516fae1
ArmyAntMF54 <anthonyjpadgett@gmail.com>
2021-04-08 14:54:03 -0500

    task done

---
+++ repos/ReturnStatementPractice/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ReturnStatementPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int iceNumber = ice(6);
+            Console.WriteLine(iceNumber);
+        }
+        static int ice(int num)
+        {
+            int result = num * num * num;
+            return result;
+        }
+
+
+
+
+
+
+
+
+    }
+}
